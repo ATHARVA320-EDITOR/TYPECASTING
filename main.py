b24592641c9062993bbc191e8f40ccaaef07187d
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-10-01 16:35:31 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Penguin"
+age = 15
+is_student =  True
+weight = 38.5
+print("Name:", name)
+print("Age:", age)
+print("is_student:", is_student)
+print("weight", weight)
+print("\n After Type Casting")
+age = str(age)
+print(age)
+print("Data type of Age is", type(age))
+weight =  int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
